aeeec65cae05c5f715e00102aa46b9916a7c63f4
harshal_s_25 <harshalshelar8251@gmail.com>
2024-12-06 05:57:09 +0000

    Second commit

--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -12,50 +12,68 @@ import { BreadcrumbComponent } from '../../shared/breadcrumb/breadcrumb.componen
 @Component({
   selector: 'app-user-form',
   standalone: true,
-  imports: [ReactiveFormsModule, OrderTotalComponent, RouterLink, BreadcrumbComponent],
+  imports: [
+    ReactiveFormsModule,
+    OrderTotalComponent,
+    RouterLink,
+    BreadcrumbComponent,
+  ],
   templateUrl: './user-form.component.html',
-  styleUrl: './user-form.component.scss'
+  styleUrl: './user-form.component.scss',
 })
 export class UserFormComponent {
-
   updateForm!: FormGroup;
   isSubmitted: boolean = false;
-  breadcrumb : any = []
+  breadcrumb: any = [];
 
-  constructor(private fb: FormBuilder, private router :Router) {}
+  constructor(private fb: FormBuilder, private router: Router) {}
 
   ngOnInit() {
     this.updateForm = this.fb.group({
       name: ['', [Validators.required, Validators.pattern('^[a-zA-Z ]*$')]],
       email: ['', [Validators.required, Validators.email]],
-      phone: ['', [Validators.required, Validators.pattern('^[0-9]*$'), Validators.maxLength(10), Validators.minLength(10)]],
-      pincode: ['', [Validators.required, Validators.pattern('^[0-9]*$'),Validators.maxLength(6), Validators.minLength(6)]],
+      phone: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('^[0-9]*$'),
+          Validators.maxLength(10),
+          Validators.minLength(10),
+        ],
+      ],
+      pincode: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('^[0-9]*$'),
+          Validators.maxLength(6),
+          Validators.minLength(6),
+        ],
+      ],
       address: ['', [Validators.required]],
       city: ['', [Validators.required, Validators.pattern('^[a-zA-Z ]*$')]],
     });
 
     this.breadcrumb = [
-      {url : '/list', name : 'User List'},
-      {url : '/cart', name : 'Cart'},
-      {url : '/userForm', name : 'Shipping Form'},
-    ]
+      { url: '/list', name: 'User List' },
+      { url: '/cart', name: 'Cart' },
+      { url: '/userForm', name: 'Shipping Form' },
+    ];
   }
 
   submitForm() {
     this.isSubmitted = true;
     console.log(this.updateForm.controls);
-    
+
     this.updateForm.markAllAsTouched();
 
     if (this.updateForm.valid) {
-      console.log("Awdawd");
-      
-        this.router.navigateByUrl('/payment')
+
+      this.router.navigateByUrl('/payment');
     }
   }
 
   get f() {
     return this.updateForm.controls;
   }
-
 }
